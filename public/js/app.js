4bde5877eef94183d71a8c5eaca25ed752d7cbf7
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2021-10-25 07:37:46 +0200

    update all files

---
+++ public/js/app.js
@@ -0,0 +1,45 @@
+
+
+const weatherForm = document.querySelector('#weatherForm');
+const search = document.getElementsByTagName('input')[0];
+const getWeather = document.querySelector('#getWeather');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+
+weatherForm.addEventListener('submit', (e) => {
+    if (search == null || search.value.length <= 0) {
+        console.log('test');
+        e.preventDefault()
+        return console.log('You Need To write any value to search!!');
+    }
+
+    messageOne.textContent = 'loading.....'
+    messageTwo.textContent = ''
+
+
+    fetch(`http://localhost:3000/weather?address=${search.value}`).then(response => {
+        console.log(response);
+        // response.json().then(data => {
+        //     console.log(data);
+        //     messageOne.textContent = data.location;
+        //     messageOne.textContent = data.temperature;
+        //     search.value = '';
+        // })
+    });
+})
+
+// getWeather.addEventListener('click', (e) => {
+//     if (search == null || search.value.length <= 0) {
+//         e.preventDefault()
+//         return console.log('You Need To write any value to search!!');
+//     }
+
+//     fetch(`http://localhost:3000/weather?address=${search.value}`).then(response => {
+//         response.json().then(data => {
+//             console.log(data);
+//             search.value = '';
+//         })
+//     });
+// })
+
---
+++ src/app.js
@@ -0,0 +1,97 @@
+//==================== To make nodemon see all changes in hbs and js files (nodemon app.js js,hbs) ===================//
+
+
+// path is local in core to detect any path of our project 
+// hbs is a backage (handle parse) to make a dynamic content 
+
+const geoCode = require('./utils/geoCode');
+const getWeather = require('./utils/weather');
+
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
+const hbs = require('hbs');
+const ejs = require('ejs');
+
+
+const publicDirPath = path.join(__dirname, '../public');
+
+
+// to use hbs 
+app.use(express.static(publicDirPath)); // this is replace app.get('/')
+
+// Setub handlebarse engine and views location
+const viewsPath = path.join(__dirname, '../template/views');
+const partialsPath = path.join(__dirname, '../template/partial');
+
+
+app.set('view engine', 'ejs')
+app.set('views', viewsPath);// Now we don't need to put views in src file (we change default path)
+// hbs.registerPartials(partialsPath);// to path more files we can use registerPartials
+
+
+// to send an file we need to use render (To path values like laravel)
+app.get('', (req, res)=> {
+    res.render('index', { // without extension
+        title : 'Weather',
+        name : 'Ibrahim Ali Mohamed Ibrahim'
+    });
+});
+
+app.get('/about', (req, res)=> {
+    res.render('about', {
+        title : 'About',
+        description : `This is from render file from hyper parse (hbs)=> it's great package but now we use {ejs} its better than`,
+        name : 'Ibrahim Ali Mohamed Ibrahim'
+    })
+});
+
+// Handle query string 
+app.get('/weather', (req, res)=>{
+    if(!req.query.address){
+        return res.send({
+            error : 'You should to write your address first!!'
+        })
+    };
+
+    console.log(req.query.address);
+    geoCode(req.query.address, (error, {longitude, latitude})=>{
+        if(error){
+            return res.send({error});
+        }
+
+        getWeather(longitude, latitude , (error, {temp, location}={})=>{
+            if(error){
+                return res.send({error});
+            };
+
+            res.send({
+                location : req.query.address,
+                Code : req.query.address.substring(0,2).toUpperCase(),
+                currentWeather : location,
+                temperature : temp,
+            })
+        })
+    })
+
+    
+})
+
+
+// Handle 404 pages 
+app.get('*', (req, res)=> {
+    res.render('404',{
+        title : '404 page',
+        name : 'Ibrahim Ali Mohamed Ibrahim',
+        errorMessage : 'Page Not Found!!!'
+    })
+})
+
+
+app.listen(port, ()=>{
+    console.log('Server Is Up On Port: ',port);
+})
+
+
+
